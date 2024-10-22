a5b397b2818f729398c840d155bc395c5eaae0d3
adhlira <adhliramadhan68@gmail.com>
2024-10-22 11:25:35 +0700

    fix validation at category name

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -20,13 +21,17 @@ class CategoryController extends Controller
 
     public function Add(Request $request)
     {
-        $validateData = $request->validate([
-            'category_name' => 'required|unique:categories,name'
+        $name = Str::lower($request->input('category_name'));
+        $category_exist = Category::whereRaw('LOWER(name) = ?', [$name])->exists();
+        $request->validate([
+            'category_name' => 'required|string'
         ]);
-
-        $category = new Category();
-        $category->name = $validateData['category_name'];
-        $category->save();
+        if ($category_exist) {
+            return back()->withErrors(['category_name' => 'The category name has already been taken.']);
+        }
+        Category::create([
+            'name' => $request->input('category_name')
+        ]);
 
         return redirect('/categories')->with('success', 'Successfully Added Data');
     }
